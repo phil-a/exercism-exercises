6d9987d073adc15d38c1f5d5ac07a8ac5351322a
Philip Alekseev <phil.alekseev@gmail.com>
2018-08-31 19:14:27 -0400

    Refactor 'Word Count' problem

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,29 +7,13 @@ defmodule Words do
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
-    |> replace_underscores()
-    |> strip_punctuation_except_hyphen()
-    |> strip_whitespace()
+    |> sanitize_sentence()
     |> String.downcase()
-    |> String.split([" ", "_"])
-    |> create_word_map()
+    |> String.split([" ", "_"], trim: true)
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
   end
 
-  defp replace_underscores(sentence), do: Regex.replace(~r/[_]+/u, sentence, " ")
-
-  defp strip_punctuation_except_hyphen(sentence),
-    do: Regex.replace(~r/[\p{P}\p{S}](?<!-)/u, sentence, "")
-
-  defp strip_whitespace(sentence), do: Regex.replace(~r/\s\s+/, sentence, " ")
-
-  defp create_word_map(list, map \\ %{})
-
-  # Base case (list is empty)
-  defp create_word_map([], map), do: map
-
-  # Recursive case (increment count and reduce list size)
-  defp create_word_map([hd | tl], map) do
-    map = Map.update(map, hd, 1, &(&1 + 1))
-    create_word_map(tl, map)
+  defp sanitize_sentence(sentence) do
+    Regex.replace(~r/[\p{P}\p{S}](?<!-)(?<!_)/u, sentence, "")
   end
 end
